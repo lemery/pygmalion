022f90967d49a4018463523ddff96da3e5560e2c
lemery <luke.a.emery@gmail.com>
2014-11-29 18:11:02 -0800

    Partly implemented consequences, fully implemented skill list, refresh, aspects, stress tracks

--- public/js/fateGame.js
+++ public/js/fateGame.js
@@ -1,5 +1,6 @@
 var numAspects = 1;
 var numSkills = 1;
+var numTracks = 1;
 
 function addAspect() {    
     var inGroup = document.createElement("div");
@@ -53,10 +54,9 @@ function removeAspect(num) {
         
         var rmBtn = document.getElementById("removeAspectButton" + x);
         rmBtn.setAttribute("id", "removeAspectButton" + t);
-        rmBtn.setAttribute("onClick", "removeAspect(" + t + ")");
-        
-        numAspects--;
+        rmBtn.setAttribute("onClick", "removeAspect(" + t + ")");        
     }
+    numAspects--;
 }
 
 function addSkill() {    
@@ -99,20 +99,120 @@ function addSkill() {
 function removeSkill(num) {
     var inGroup = document.getElementById("skillInputGroup" + num);
     inGroup.parentNode.removeChild(inGroup);
-    for (x = num + 1; x <= numSkills; x++) {
+    for (x = num + 1; x < numSkills; x++) {
         var t = x-1;
         
         var inG = document.getElementById("skillInputGroup" + x);
+        console.log("tried to set skillInputGroup" + x)
         inG.setAttribute("id", "skillInputGroup" + t);
         
         var skillName = document.getElementById("skillName" + x);
-        skilName.setAttribute("id", "skillName" + t);
+        skillName.setAttribute("id", "skillName" + t);
         skillName.setAttribute("name", "skillName" + t);
         
         var rmBtn = document.getElementById("removeSkillButton" + x);
         rmBtn.setAttribute("id", "removeSkillButton" + t);
-        rmBtn.setAttribute("onClick", "removeSkill(" + t + ")");
-        
-        numSkills--;
+        rmBtn.setAttribute("onClick", "removeSkill(" + t + ")");       
+        console.log("Changed skill " + x + " to " + t);
     }
+    numSkills--;
+}
+
+function addTrack() {
+	var inGroupA = document.createElement("div");
+	inGroupA.setAttribute("class", "input-group");
+	
+	var nameLabel = document.createElement("span");
+	nameLabel.setAttribute("class", "input-group-addon");
+	nameLabel.appendChild(document.createTextNode("Name"));
+	
+	var nameField = document.createElement("input");
+	nameField.setAttribute("class", "form-control");
+	nameField.setAttribute("type", "text");
+	nameField.setAttribute("name", "stressTrack" + numTracks + "name");
+	nameField.setAttribute("id", "stressTrack" + numTracks + "name");
+	
+	var inGroupB = document.createElement("div");
+	inGroupB.setAttribute("class", "input-group");
+	
+	var numLabel = document.createElement("span");
+	numLabel.setAttribute("class", "input-group-addon");
+	numLabel.appendChild(document.createTextNode("# of Boxes"));
+	
+	var numBoxes = document.createElement("span");
+	numBoxes.setAttribute("class", "form-control");
+	numBoxes.setAttribute("type", "text");
+	numBoxes.setAttribute("name", "stressTrack" + numTracks + "size");
+	numBoxes.setAttribute("id", "stressTrack" + numTracks + "size");
+	
+    var removeSpan = document.createElement("span");
+    removeSpan.setAttribute("class", "input-group-btn");
+	
+    var removeButton = document.createElement("button");
+    removeButton.setAttribute("class", "btn btn-default");
+    removeButton.setAttribute("type", "button");
+    removeButton.setAttribute("onClick", "removeTrack(" + numTracks + ")");
+    removeButton.setAttribute("id", "removeTrackButton" + numTracks);
+    removeButton.appendChild(document.createTextNode("Remove"));
+    
+    var row = document.createElement("div");
+    row.setAttribute("class", "row");
+    row.setAttribute("id", "stress_track_row_" + numTracks);
+    
+    var col1 = document.createElement("div");
+    col1.setAttribute("class", "col-lg-6");
+    
+    var col2 = document.createElement("div");
+    col2.setAttribute("class", "col-lg-4");
+    
+    var col3 = document.createElement("div");
+    col3.setAttribute("class", "col-lg-2");
+	
+    inGroupA.appendChild(nameLabel);
+    inGroupA.appendChild(nameField);
+    
+    inGroupB.appendChild(numLabel);
+    inGroupB.appendChild(numBoxes);
+    
+    removeSpan.appendChild(removeButton);
+    
+    col1.appendChild(inGroupA);
+    col2.appendChild(inGroupB);
+    col3.appendChild(removeSpan);
+    
+    row.appendChild(col1);
+    row.appendChild(col2);
+    row.appendChild(col3);
+	
+    var body = document.getElementById("stress_tracks_body");
+    var btn = document.getElementById("add_track_button");
+    body.insertBefore(row, btn);
+    
+    numTracks++;
+}
+
+function removeTrack(num) {
+    var row = document.getElementById("stress_track_row_" + num);
+    row.parentNode.removeChild(row);
+    for (x = num + 1; x < numTracks; x++) {
+        var t = x-1;
+        
+        var nRow = document.getElementById("stress_track_row_" + x);
+        nRow.setAttribute("id", "stress_track_row_" + t);
+        
+        var nameField = document.getElementById("stressTrack" + x + "name");
+        nameField.setAttribute("name", "stressTrack" + t + "name");
+        nameField.setAttribute("id", "stressTrack" + t + "name");
+        console.log("changed nameField ")
+        
+        var numBoxes = document.getElementById("stressTrack" + x + "size")
+        numBoxes.setAttribute("name", "stressTrack" + t + "size");
+        numBoxes.setAttribute("id", "stressTrack" + t + "size");
+        
+        var rmBtn = document.getElementById("removeTrackButton" + x);
+        rmBtn.setAttribute("id", "removeTrackButton" + t);
+        rmBtn.setAttribute("onClick", "removeTrack(" + t + ")");
+        
+    }
+    numTracks--;
 }
